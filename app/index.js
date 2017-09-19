c1cb76fe7d3b5405ade6e98f9cae095e6703360e
qighliu29 <qighliu29@gmail.com>
2017-09-19 12:30:40 +0800

    Tree Shaking

--- app/index.js
+++ app/index.js
@@ -2,5 +2,7 @@ import component from './component';
 import './main.css';
 import 'purecss';
 import 'font-awesome/css/font-awesome.css';
+import { bake } from './shake';
 
+bake();
 document.body.appendChild(component());
